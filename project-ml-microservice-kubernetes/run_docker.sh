6f0ebdb62fcefa75c80725f695a17bacad62500f
EC2 Default User <ec2-user@ip-172-31-9-145.us-east-2.compute.internal>
2021-04-02 09:57:32 +0000

    ml microservices

--- project-ml-microservice-kubernetes/run_docker.sh
+++ project-ml-microservice-kubernetes/run_docker.sh
@@ -4,9 +4,12 @@
 
 # Step 1:
 # Build image and add a descriptive tag
+docker image build -t microservicesml:1.1 .
 
 # Step 2: 
 # List docker images
+docker image ls
 
 # Step 3: 
 # Run flask app
+docker container run --publish 8000:8000 --name microsml microservicesml:1.1
--- project-ml-microservice-kubernetes/upload_docker.sh
+++ project-ml-microservice-kubernetes/upload_docker.sh
@@ -5,11 +5,15 @@
 
 # Step 1:
 # Create dockerpath
-# dockerpath=<your docker ID/path>
+dockerpath=satya12345/microservicesml
 
 # Step 2:  
 # Authenticate & tag
+export DOCKER_ID_USER="satya12345"
+docker login
+docker tag microservicesml:1.1 $DOCKER_ID_USER/microservicesml
 echo "Docker ID and Image: $dockerpath"
 
 # Step 3:
 # Push image to a docker repository
+docker push $dockerpath
