d68e9ff442039bb653180e1890c01b61ce62b0c0
EC2 Default User <ec2-user@ip-172-31-9-145.us-east-2.compute.internal>
2021-04-02 10:22:31 +0000

    adding minikube and circle ci config

--- project-ml-microservice-kubernetes/run_kubernetes.sh
+++ project-ml-microservice-kubernetes/run_kubernetes.sh
@@ -5,14 +5,16 @@
 # Step 1:
 # This is your Docker ID/path
 # dockerpath=<>
+dockerpath=satya12345/microservicesml
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl run mcrspredict --port=9000 --image=$dockerpath
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
-
+kubectl port-forward deployment.apps/mcrspredict 9000:8000
